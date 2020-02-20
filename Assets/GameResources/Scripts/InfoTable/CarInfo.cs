7588b62562a56e2f1d53244a8f27b0a2fb9f7cd0
HEE-SUK <dltjr5177@gmail.com>
2020-02-20 15:27:18 +0900

    구글 수정

--- Assets/GameResources/Scripts/InfoTable/CarInfo.cs
+++ Assets/GameResources/Scripts/InfoTable/CarInfo.cs
@@ -13,10 +13,14 @@
 
     public int def {get;}
     public int mSpd {get;}
-    public int rhm {get;}
+    public float rhm {get;}
     public int atk {get;}
+    public float atkSpd {get;}
+    public int bullet {get;}
     public int reload {get;}
+
     public int price {get;}
+    public int convertPrice {get;}
 
     public UNITTYPE unitType {get;}
     public float lotto {get;}
@@ -37,7 +41,11 @@
         this.mSpd = _data.Mspd;
         this.rhm = _data.Rhm;
         this.atk = _data.Atk;
+        this.atkSpd = _data.Atkspd;
+        this.bullet = _data.Bullet;
         this.reload = _data.Reload;
+
+        this.convertPrice = _data.Convertprice;
         this.price = _data.Price;
         
         this.unitType = _data.UNITTYPE;
