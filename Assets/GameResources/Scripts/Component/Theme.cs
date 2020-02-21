4c34204794f87abaae9624a92fb4b2d11761cf7b
WiSeoungHwan <dnltjdghks1@gmail.com>
2020-02-21 15:55:21 +0900

    close #8 반대쪽 맵 생성 및 플레이어 프리펩 Car 로 이름변경 및 하이어라키상에 추가

--- Assets/GameResources/Scripts/Component/Theme.cs
+++ Assets/GameResources/Scripts/Component/Theme.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Theme : MonoBehaviour
 {
     [SerializeField] private GameObject[] wall = null;
-    [SerializeField] private GameObject[] house = null;
+    [SerializeField] private GameObject[] leftHouse = null;
+    [SerializeField] private GameObject[] rightHouse = null;
+
     [SerializeField] private GameObject[] powerpole = null;
     [SerializeField] private GameObject[] road = null; // 0: 신호 표시 있는거, 1: 신호표시 없는거
-    [SerializeField] private GameObject[] trafficlight;
-    [SerializeField] private GameObject[] floor = null;
-    [SerializeField] private GameObject[] ground = null;
+    [SerializeField] private GameObject[] leftTrafficlight;
+    [SerializeField] private GameObject[] rightTrafficlight;
+    [SerializeField] private GameObject[] leftFloor = null;
+    [SerializeField] private GameObject[] rightFloor = null;
+    [SerializeField] private GameObject[] leftGround = null;
+    [SerializeField] private GameObject[] rightGround = null;
     [SerializeField] private GameObject[] options = null;
     [SerializeField] private Transform[] optionPos = null;
 
@@ -18,18 +23,25 @@ public class Theme : MonoBehaviour
         
         // 세팅 전 비활성화
         DisableObjectArr(wall);
-        DisableObjectArr(house);
+        DisableObjectArr(leftHouse);
         DisableObjectArr(powerpole);
+        DisableObjectArr(rightHouse);
         DisableObjectArr(road);
-        DisableObjectArr(floor);
-        DisableObjectArr(ground);
+        DisableObjectArr(leftFloor);
+        DisableObjectArr(rightFloor);
+        DisableObjectArr(leftGround);
+        DisableObjectArr(rightGround);
         DisableObjectArr(options);
-        DisableObjectArr(trafficlight);
+        DisableObjectArr(leftTrafficlight);
+        DisableObjectArr(rightTrafficlight);
         // 랜덤으로 객체 활성
-        house[Random.Range(0,house.Length)].SetActive(true);
+        leftHouse[Random.Range(0,leftHouse.Length)].SetActive(true);
+        leftFloor[Random.Range(0,leftFloor.Length)].SetActive(true);
+        leftGround[Random.Range(0,leftGround.Length)].SetActive(true);
+        rightHouse[Random.Range(0,rightHouse.Length)].SetActive(true);
+        rightFloor[Random.Range(0,rightFloor.Length)].SetActive(true);
+        rightGround[Random.Range(0,rightGround.Length)].SetActive(true);
         powerpole[Random.Range(0,powerpole.Length)].SetActive(true);
-        floor[Random.Range(0,floor.Length)].SetActive(true);
-        ground[Random.Range(0,ground.Length)].SetActive(true);
         // 벽 생성
         if(settingData.isWallActive){
             int wallIndex = settingData.wallIndex < wall.Length ? settingData.wallIndex : wall.Length - 1;
@@ -38,12 +50,15 @@ public class Theme : MonoBehaviour
                 Debug.Log("this object have not WallComponent");
             activeWall.Init(settingData.wallInfo);
             wall[wallIndex].SetActive(true);
+        }else{
+
         }
         
         // 신호 표시 있는 길과 없는길 규칙 적용 
         if(settingData.isCrossWalk){
             road[0].SetActive(true);
-            trafficlight[Random.Range(0,road.Length)].SetActive(true);
+            leftTrafficlight[Random.Range(0,road.Length)].SetActive(true);
+            rightTrafficlight[Random.Range(0,road.Length)].SetActive(true);
         }else{
             road[Random.Range(1,road.Length)].SetActive(true);
         }
