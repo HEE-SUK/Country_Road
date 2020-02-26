442c228d3107d29df8e3c3b6750e7b100b29bce5
WiSeoungHwan <dnltjdghks1@gmail.com>
2020-02-26 18:09:24 +0900

    프리펩 데이터 적용

---
+++ Assets/GameResources/Scripts/Component/EnemyMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 0;
+
+    public void Init(float speed){
+        this.speed = speed;
+    }
+
+    void Update(){
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/GameResources/Scripts/Controller/EnemyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] private EnemyMovement enemyMov;
+    private ZombieInfo zombieInfo;
+    
+    public void Init(ZombieInfo zombieInfo){
+        this.zombieInfo = zombieInfo;
+        enemyMov.Init(GameManager.GameSpeed - speed);
+    }
+}
