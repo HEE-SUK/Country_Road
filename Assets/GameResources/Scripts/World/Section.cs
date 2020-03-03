5fadbbd661335c863158c5a0929efd0da4c732c7
HEE-SUK <dltjr5177@gmail.com>
2020-03-03 22:46:10 +0900

    프리팹 수정

---
+++ Assets/GameResources/Scripts/World/Section.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Section : MonoBehaviour
+{
+    private SectionColumn[] columns = {};
+    private void Awake()
+    {
+        this.columns = this.GetComponentsInChildren<SectionColumn>();
+    }
+    private void Start()
+    {
+        this.Init();
+    }
+    public void Init()
+    {
+        // 순서대로 배치
+        this.StartCoroutine(this.StartInit());
+    }
+
+    private IEnumerator StartInit()
+    {
+        foreach (var column in this.columns)
+        {
+            column.Init();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
---
+++ Assets/GameResources/Scripts/World/SectionRow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SectionRow : MonoBehaviour
+{
+    public void Init()
+    {
+        this.transform.DOLocalMoveY(0f, 0.5f).SetEase(Ease.OutBack);
+    }
+}
